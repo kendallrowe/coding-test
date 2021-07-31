8e98b459171deeb22b3febd3cabbd787e2387cca
Kendall Rowe <kendall.rowe312@gmail.com>
2021-07-30 20:21:03 -0400

    Configuring server for heroku deployment

--- api-server/index.js
+++ api-server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 
@@ -12,11 +13,16 @@ const cats = require("./routes/cats");
 const sharks = require("./routes/sharks");
 
 app.listen(PORT, () => {
+    app.use(express.static(path.resolve(__dirname, '../client/build')));
     app.use(cors());
     app.use(helmet());
 
     app.use("/api", cats(db));
     app.use("/api", sharks(db));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+    });
 
     console.log(`Hi there! I'm listening on ${PORT}`);
 });
