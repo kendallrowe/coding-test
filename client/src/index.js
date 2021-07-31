c481fdf58f0af567e08451036660dd2c1e2952c0
Kendall Rowe <kendall.rowe312@gmail.com>
2021-07-30 20:41:20 -0400

    Removed use of normalize css from node modules for heroku build

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import "./styles/normalize.scss";
 import "./index.scss";
+
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
